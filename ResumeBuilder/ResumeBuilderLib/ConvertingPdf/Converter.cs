0750e488fc3715b23098cc64adb0dc55112ca6cb
ittybe <misha.kan7.2004.20@gmail.com>
2020-05-20 20:14:25 +0300

    Add Converter, add wpf project

---
+++ ResumeBuilder/ResumeBuilderLib/ConvertingPdf/Converter.cs
@@ -0,0 +1,37 @@
+﻿using Spire.Doc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ResumeBuilderLib.ConvertingPdf
+{
+    public class Converter
+    {
+        public Document Document { get; set; }
+
+        public Converter(string filepath, FileFormat fileFormat) 
+        {
+            Document = new Document(filepath, fileFormat);
+        }
+
+        public void Convert(string output, FileFormat fileFormat) 
+        {
+            Document.SaveToFile(output, fileFormat);
+        }
+
+        public void ConvertToImage(FileInfo output, Spire.Doc.Documents.ImageType imageType) 
+        {
+            //for (int i = 1; i < Document.PageCount; i++)
+            //{
+            //    //int indexPoint = output.Name.LastIndexOf('.');
+            //    //string Name;
+            //    //if (indexPoint >= 0)
+            //    //    Name = output.Name.Substring(0, indexPoint) + i;
+            //    //else
+            //    //    Name = output.Name + i;
+            //}
+            Document.SaveToImages(0, Document.PageCount, imageType);
+        }
+    }
+}
